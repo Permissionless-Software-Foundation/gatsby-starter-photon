1a3261f6fb358e555c963c265fd943763a07ad17
Chris Troutner <chris.troutner@gmail.com>
2018-12-30 14:29:47 -0800

    Added QR code

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import pic01 from '../assets/images/pic01.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
+import qrcode from '../assets/images/qrcode.png'
 
 class Homepage extends React.Component {
     render() {
@@ -28,8 +29,11 @@ class Homepage extends React.Component {
                             </header>
                             <p>
                               The mission of the Permissionless Software
-                              Foundation is to promote individual sovereignty
-                              through the use of software. The Foundation will
+                              Foundation is to
+                              promote <a href="https://en.wikipedia.org/wiki/Self-ownership" target="_blank">
+                                individual sovereignty
+                              </a> through
+                              the use of software. The Foundation will
                               focus on the development and promotion of software
                               that makes it easy for individuals to protect
                               their privacy, circumvent censorship, and engage
@@ -58,19 +62,20 @@ class Homepage extends React.Component {
                 <section id="two" className="main style2">
                     <div className="grid-wrapper">
                         <div className="col-6">
-                            <ul className="major-icons">
-                                <li><span className="icon style1 major fa-code"></span></li>
-                                <li><span className="icon style2 major fa-bolt"></span></li>
-                                <li><span className="icon style3 major fa-camera-retro"></span></li>
-                                <li><span className="icon style4 major fa-cog"></span></li>
-                                <li><span className="icon style5 major fa-desktop"></span></li>
-                                <li><span className="icon style6 major fa-calendar"></span></li>
-                            </ul>
+                          <center>
+                            <span className="image">
+                              <img src={qrcode} alt="" />
+                            </span>
+                            <p>
+                              bitcoincash:qzl6k0wvdd5ky99hewghqdgfj2jhcpqnfq8xtct0al
+                            </p>
+                          </center>
                         </div>
                         <div className="col-6">
                             <header className="major">
-                                <h2>Lorem ipsum dolor adipiscing<br />
-                                amet dolor consequat</h2>
+                                <h2>
+                                  Buy or Sell PSF Tokens
+                                </h2>
                             </header>
                             <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
                             <p>Blandit faucibus proin. Ac aliquam integer adipiscing enim non praesent vis commodo nunc phasellus cubilia ac risus accumsan. Accumsan blandit. Lobortis phasellus non lobortis dit varius mi varius accumsan lobortis. Blandit ante aliquam lacinia lorem lobortis semper morbi col faucibus vitae integer placerat accumsan orci eu mi odio tempus adipiscing adipiscing adipiscing curae consequat feugiat etiam dolore.</p>
