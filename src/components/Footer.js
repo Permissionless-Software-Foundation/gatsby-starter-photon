12555124d39736c14b4ff20788de84443a4e6f9c
Chris Troutner <chris.troutner@gmail.com>
2018-12-30 18:11:05 -0800

    Added first couple software projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                     <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                     <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                     <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                    <li><a href="https://github.com/Permissionless-Software-Foundation" target="_blank" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                     <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import Helmet from "react-helmet"
+import React from 'react'
+import Helmet from 'react-helmet'
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
@@ -18,151 +18,172 @@ const BchAddress = styled.p`
 `
 
 class Homepage extends React.Component {
-    render() {
-        const siteTitle = "Permissionless Software Foundation";
+  render() {
+    const siteTitle = 'Permissionless Software Foundation'
 
-        return (
-            <Layout>
-                <Helmet title={siteTitle} />
-                <section id="one" className="main style1">
-                    <div className="grid-wrapper">
-                        <div className="col-6">
-                            <header className="major">
-                                <h2>
-                                  Permissionless<br />
-                                  &nbsp;Software<br />
-                                  &nbsp;&nbsp;Foundation
-                                </h2>
-                            </header>
-                            <p>
-                              The mission of the Permissionless Software
-                              Foundation is to
-                              promote <a href="https://en.wikipedia.org/wiki/Self-ownership" target="_blank">
-                                individual sovereignty
-                              </a> through
-                              the use of software. The Foundation will
-                              focus on the development and promotion of software
-                              that makes it easy for individuals to protect
-                              their privacy, circumvent censorship, and engage
-                              in <a href="https://en.wikipedia.org/wiki/Agorism" target="_blank">Agorism</a>.
-                            </p>
-                        </div>
-                        <div className="col-6">
-                          <center>
-                            <p>
-                              The business plan for the Permissionless Softare Foundation
-                              and PSF token is still being drafted. You can read the
-                              current draft by clicking on the button below. It discusses
-                              the unique token economics and mathematics used to create a
-                              psudo-stable utility token allowing open source software
-                              communities to self-fund without the need for venture capital,
-                              corporate sponshorship, or other fiduciary responsibility.
-                            </p>
-                            <a href="https://docs.google.com/document/d/1UgX_h4TB9CtxxabseC7lRGwQlCsNikPo7DJUlIrNv0k/edit?usp=sharing" target="_blank" className="button special">
-                              Read the Business Plan
-                            </a>
-                          </center>
-                        </div>
-                    </div>
-                </section>
+    return (
+      <Layout>
+        <Helmet title={siteTitle} />
+        <section id="one" className="main style1">
+          <div className="grid-wrapper">
+            <div className="col-6">
+              <header className="major">
+                <h2>
+                  Permissionless
+                  <br />
+                  &nbsp;Software
+                  <br />
+                  &nbsp;&nbsp;Foundation
+                </h2>
+              </header>
+              <p>
+                The mission of the Permissionless Software Foundation is to
+                promote{' '}
+                <a
+                  href="https://en.wikipedia.org/wiki/Self-ownership"
+                  target="_blank"
+                >
+                  individual sovereignty
+                </a>{' '}
+                through the use of software. The Foundation will focus on the
+                development and promotion of software that makes it easy for
+                individuals to protect their privacy, circumvent censorship, and
+                engage in{' '}
+                <a href="https://en.wikipedia.org/wiki/Agorism" target="_blank">
+                  Agorism
+                </a>
+                .
+              </p>
+            </div>
+            <div className="col-6">
+              <center>
+                <p>
+                  The business plan for the Permissionless Softare Foundation
+                  and PSF token is still being drafted. You can read the current
+                  draft by clicking on the button below. It discusses the unique
+                  token economics and mathematics used to create a psudo-stable
+                  utility token allowing open source software communities to
+                  self-fund without the need for venture capital, corporate
+                  sponshorship, or other fiduciary responsibility.
+                </p>
+                <a
+                  href="https://docs.google.com/document/d/1UgX_h4TB9CtxxabseC7lRGwQlCsNikPo7DJUlIrNv0k/edit?usp=sharing"
+                  target="_blank"
+                  className="button special"
+                >
+                  Read the Business Plan
+                </a>
+              </center>
+            </div>
+          </div>
+        </section>
 
-                <section id="two" className="main style2">
-                    <div className="grid-wrapper">
-                        <div className="col-6">
-                          <center>
-                            <span className="image">
-                              <img src={qrcode} alt="" />
-                            </span>
-                            <BchAddress>
-                              bitcoincash:qzl6k0wvdd5ky99hewghqdgfj2jhcpqnfq8xtct0al
-                            </BchAddress>
-                            <BadgerButton />
-                          </center>
-                        </div>
-                        <div className="col-6">
-                          <header className="major">
-                              <h2>
-                                Buy or Sell PSF Tokens
-                              </h2>
-                          </header>
-                          <p>
-                            You can buy or sell tokens by sending BCH or tokens
-                            to the liquidity app. Scan the QR code with a
-                            Wormhole token-aware BCH wallet, like
-                            the <a href="https://wallet.wormhole.cash/" target="_blank">
-                            Wormhole Android wallet
-                            </a>. Or install
-                            the <a href="https://badgerwallet.cash/" target="_blank">
-                            Badger Wallet
-                            </a> browser extension and click the button.
-                          </p>
-                          <p>
-                            The liquidity app will send tokens if it recieves
-                            BCH. It will send BCH if it recieves tokens.
-                            Refresh the window to get the most up-to-date exchange
-                            rate.
-                          </p>
-                          <p>
-                            <b><u>Warning:</u></b> Do not send BCH to the app address
-                            unless you are using a Wormhole token-aware wallet.
-                          </p>
+        <section id="two" className="main style2">
+          <div className="grid-wrapper">
+            <div className="col-6">
+              <center>
+                <span className="image">
+                  <img src={qrcode} alt="" />
+                </span>
+                <BchAddress>
+                  bitcoincash:qzl6k0wvdd5ky99hewghqdgfj2jhcpqnfq8xtct0al
+                </BchAddress>
+                <BadgerButton />
+              </center>
+            </div>
+            <div className="col-6">
+              <header className="major">
+                <h2>Buy or Sell PSF Tokens</h2>
+              </header>
+              <p>
+                You can buy or sell tokens by sending BCH or tokens to the
+                liquidity app. Scan the QR code with a Wormhole token-aware BCH
+                wallet, like the{' '}
+                <a href="https://wallet.wormhole.cash/" target="_blank">
+                  Wormhole Android wallet
+                </a>
+                . Or install the{' '}
+                <a href="https://badgerwallet.cash/" target="_blank">
+                  Badger Wallet
+                </a>{' '}
+                browser extension and click the button.
+              </p>
+              <p>
+                The liquidity app will send tokens if it recieves BCH. It will
+                send BCH if it recieves tokens. Refresh the window to get the
+                most up-to-date exchange rate.
+              </p>
+              <p>
+                <b>
+                  <u>Warning:</u>
+                </b>
+                Do not send BCH to the app address unless you are using a
+                Wormhole token-aware wallet.
+              </p>
+            </div>
+          </div>
+        </section>
 
-                        </div>
-                    </div>
-                </section>
+        <section id="three" className="main style1 special">
+          <div className="grid-wrapper">
+            <div className="col-12">
+              <header className="major">
+                <h2>Software Projects</h2>
+              </header>
+              <p>
+                The PSF token will be used to fund the following open source
+                projects.
+              </p>
+            </div>
 
-                <section id="three" className="main style1 special">
-                    <div className="grid-wrapper">
-                        <div className="col-12">
-                            <header className="major">
-                                <h2>Adipiscing amet consequat</h2>
-                            </header>
-                            <p>Ante nunc accumsan et aclacus nascetur ac ante amet sapien sed.</p>
-                        </div>
-
-                        <div className="col-4">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
-                            <ul className="actions">
-                                <li><a href="#" className="button">More</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-4">
-                            <span className="image fit"><img src={pic03} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
-                            <ul className="actions">
-                                <li><a href="#" className="button">More</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-4">
-                            <span className="image fit"><img src={pic04} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
-                            <ul className="actions">
-                                <li><a href="#" className="button">More</a></li>
-                            </ul>
-                        </div>
-
-                    </div>
-                </section>
-
-                <section id="four" className="main style2 special">
-                    <div className="container">
-                        <header className="major">
-                            <h2>Ipsum feugiat consequat?</h2>
-                        </header>
-                        <p>Sed lacus nascetur ac ante amet sapien.</p>
-                        <ul className="actions uniform">
-                            <li><a href="#" className="button special">Sign Up</a></li>
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
-                    </div>
-                </section>
-            </Layout>
-        );
-    }
+            <div className="col-6">
+              <h3>Consolidating CoinJoin</h3>
+              <p>
+                Consolidating CoinJoin is a peer-to-peer mixing service that
+                brings privacy and fungability to the Bitcoin Cash network.
+                Users will use the PSF token to pay for the mixing service.
+                Servers will use the PSF token to pay Mirrors to advertise their
+                tor .onion address on the IPFS peer-to-peer network, and also to
+                transfer data between the <i>clearnet</i>
+                and the <i>darkweb</i>.
+              </p>
+              <ul className="actions">
+                <li>
+                  <a
+                    href="https://gist.github.com/christroutner/8d54597da652fe2affa5a7230664bc45"
+                    target="_blank"
+                    className="button"
+                  >
+                    More Info
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <h3>P2P VPS: Peer-to-Peer Cloud Infrastructure</h3>
+              <p>
+                P2P VPS allows people to rent out unused computing power for
+                hosting common internet services like blogs, email, VPN, and
+                other cloud services. Any device capable of running Docker can
+                be rented on the network, in exchange for the PSF token.
+              </p>
+              <ul className="actions">
+                <li>
+                  <a
+                    href="https://p2pvps.org/about"
+                    target="_blank"
+                    className="button"
+                  >
+                    More Info
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
 }
 
-export default Homepage;
+export default Homepage
