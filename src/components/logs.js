5ddcf74ee39189d72a14ce5ea7afb1813e94fcb1
Chris Troutner <chris.troutner@gmail.com>
2019-01-12 12:02:39 -0800

    Got logs to display better in testnet page

--- src/components/logs.js
+++ src/components/logs.js
@@ -11,6 +11,8 @@ const TextArea = styled.textarea`
   font-size: 14px;
   height: 600px;
 `
+// Flag to indicate if component has had it's first render.
+let renderCnt = 0
 
 class Logs extends React.Component {
   constructor(props) {
@@ -19,16 +21,18 @@ class Logs extends React.Component {
       logStr: "Loading logs..."
     }
 
-    /*
+    // Update the logs periodically.
     setInterval(() => {
       this.getLogs()
     }, 10000)
-    */
+
+    // Initialize the logs
     this.getLogs()
   }
 
   render() {
-    return <TextArea readOnly value={this.state.logStr}>
+    renderCnt++
+    return <TextArea id="logTextArea" readOnly value={this.state.logStr}>
     </TextArea>
   }
 
@@ -38,17 +42,36 @@ class Logs extends React.Component {
       const resp = await fetch(`${SERVER}/logs`)
       const body = await resp.json()
 
-      //debugger
+      const logAry = body.logs.split('\n')
 
-      //const logData = JSON.parse(body)
-      //console.log(`body: ${JSON.stringify(logData,null,2)}`)
+      // Parse the raw logs.
+      let logStr = ""
+      for(let i=0; i < logAry.length; i++) {
+        const thisLog = logAry[i]
+        try {
+          const parsedLog = JSON.parse(thisLog)
+          logStr += `${parsedLog.message}\n`
+        } catch(err) {
+          console.log(`Error parsing: ${thisLog}`)
+          continue
+        }
+      }
 
+      // Update the state which displays new logs.
       this.setState(prevState => ({
-        logStr: body.logs
+        logStr: logStr
       }))
+
+      if(renderCnt < 3) this.scrollToBottom()
     } catch(err) {
       console.log(`Error in getLogs: `, err)
     }
+  }
+
+  // Scrolls the textarea to the bottom, most recent logs.
+  scrollToBottom() {
+    const textarea = document.getElementById('logTextArea');
+    textarea.scrollTop = textarea.scrollHeight;
   }
 }
 
